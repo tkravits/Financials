42702309a90270e713eb21775c9efcdcea01e9fb
tkravits <taylor.kravits@gmail.com>
2020-07-12 11:38:10 -0600

    Updating code

--- Yahoo_Pull.py
+++ Yahoo_Pull.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import mplfinance as mpf
 import talib
 import yfinance as yf
+
 stock = yf.download('TLRY', '2020-1-1','2020-07-07')
-stock.reset_index(level=0, inplace=True)
-stock.Date = pd.to_datetime(stock.Date, format='%Y-%m-%d')
-stock.set_index('Date', inplace=True)
+stock['Date'] = stock.index
+stock['Date'] = pd.to_datetime(stock['Date'], format='%Y-%m-%d')
+stock = stock[['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
+#stock.set_index('Date', inplace=True, drop=True)
 
 inputs = {
     'open': stock['Open'],
@@ -25,7 +27,7 @@ upper, middle, lower = talib.BBANDS(close, matype=MA_Type.T3)
 
 output = talib.MOM(close, timeperiod=5)
 
-mpf.plot(stock)
+mpf.plot(stock[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']])
 # TODO - plotting error occurs, it's looking for a datetime column, but the datetime is set as a index, not a column
 plt.plot(upper)
 plt.plot(middle)
