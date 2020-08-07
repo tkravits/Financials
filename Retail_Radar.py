9f0a705c42e8afc35a19c87c22d3729c63450a51
tkravits <taylor.kravits@gmail.com>
2020-08-07 15:42:44 -0600

    Found out the the top movers was only for s&p500, submitted issue and top movers is now included as an s&p500 and the total top movers, did a little reformating as well

--- Retail_Radar.py
+++ Retail_Radar.py
@@ -1,13 +1,22 @@
-import robin_stocks as rh
+import robin_stocks as r
 import json
 import pandas as pd
 
 with open(r'credentials.txt') as f:
-  data = json.load(f)
+    data = json.load(f)
 
-login = rh.authentication.login(data)
+login = r.authentication.login(data)
 
-top_movers = rh.markets.get_top_movers(direction='up')
+top_movers_sp500 = r.markets.get_top_movers_sp500('up')
+top_movers_all = r.markets.get_top_movers()
 
-meme = pd.DataFrame(top_movers)
+sp500_movers = pd.DataFrame(top_movers_sp500)
 
+meme = pd.DataFrame(top_movers_all)
+cols = ['ask_price', 'bid_price', 'last_trade_price', 'last_extended_hours_trade_price', 'previous_close',
+        'adjusted_previous_close']
+meme[cols] = meme[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+meme['Direction Percentage'] = ((meme['last_trade_price'] - meme['previous_close'])
+                                / meme['previous_close'])*100
+meme = meme[['symbol', 'Direction Percentage', 'ask_price', 'bid_price', 'last_trade_price',
+             'last_extended_hours_trade_price', 'previous_close', 'adjusted_previous_close']]
